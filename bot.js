9d92e0d04f33a2a3e48669828edd52c5c6f5eeb4
sorosh123 <38800644+sorosh123@users.noreply.github.com>
2019-02-08 15:55:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,15 @@
-// Extract the required classes from the discord.js module
-const { Client, Attachment } = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// Create an instance of a Discord client
-const client = new Client();
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-    // If the message is '!rip'
-    if (message.content === '!rip') {
-        // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        // Send the attachment in the message channel
-        message.channel.send(attachment);
-    }
+    if (message.content === 'ping') {
+    	message.reply('https://i.imgur.com/w3duR07.png');
+  	}
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
